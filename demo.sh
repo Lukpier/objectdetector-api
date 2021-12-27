e97ad9d9d17fd8298317d6f10dddfc9c84be92d9
Luca Pierri <luca.pierri@leitha.eu>
2021-12-27 17:58:02 +0100

    fix demo

--- demo.sh
+++ demo.sh
@@ -7,10 +7,10 @@ OUTPUT_DIR=${OUTPUT_DIR:-/tmp/output}
 echo "Starting ObjectDetector on http://$APP_HOST:$APP_PORT"
 docker container rm objectdetector
 docker build -t objectdetector .
-docker run -v $OUTPUT_DIR:$OUTPUT_DIR -p$APP_PORT:$APP_PORT -e APP_PORT=$APP_PORT -e APP_HOST=$APP_HOST -e OUTPUT_DIR=$OUTPUT_DIR-d --name objectdetector objectdetector
+docker run -v $OUTPUT_DIR:$OUTPUT_DIR -p $APP_PORT:$APP_PORT -e APP_PORT=$APP_PORT -e APP_HOST=$APP_HOST -e OUTPUT_DIR=$OUTPUT_DIR -d --name objectdetector objectdetector
 
 echo "Waiting for service to be up and running...."
-sleep 20
+sleep 10
 for filename in ./tests/input/*.jpg; do
     echo "Extracting metadata from $filename"
     curl -X POST "http://$APP_HOST:$APP_PORT/api/predict?debug=true" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "image=@$filename;type=image/jpg" -w "\nTime to Connect: %{time_connect}; Time to Transfer: %{time_starttransfer}; Time Total: %{time_total}\n"
